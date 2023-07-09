328c84a04fd0bf987d52978f4d94b8382714ec20
mellomarziano <mellomarziano@ELIUORTEGA096E>
2023-07-09 17:32:40 -0400

    Add project files.

---
+++ OrderManager/App/app.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+    angular.module("app", ["ngRoute"]).config([
+        "$routeProvider",
+        function ($routeProvider) {
+            $routeProvider
+                .when("/", {
+                    controller: "homeCtrl",
+                    templateUrl: "/app/template/home-view.html",
+                })
+                .otherwise({ redirecTo: "/" });
+        },
+    ]);
+})();
